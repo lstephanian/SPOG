9edc29f2c670f6792997e46bb90c992099e59764
Gui daCosta <gui@panteracapital.com>
2023-10-27 19:43:15 -0300

    Call smart contract vote function using wallet

--- frontend/web/public/custom/wallet.js
+++ frontend/web/public/custom/wallet.js
@@ -1,9 +1,10 @@
 import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi'
-import { watchAccount, disconnect, getAccount, signMessage } from '@wagmi/core'
+import { getContract, prepareWriteContract, writeContract, disconnect, getAccount, signMessage } from '@wagmi/core'
 import { mainnet, arbitrum } from '@wagmi/core/chains'
 
 // 1. Define constants
-const projectId = import.meta.env.VITE_PROJECT_ID
+const projectId = import.meta.env.VITE_WALLETCONNECT_PROJECT_ID
+const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS
 
 // 2. Create wagmiConfig
 const metadata = {
@@ -22,15 +23,26 @@ const modal = createWeb3Modal({ wagmiConfig, projectId, chains, defaultChain, th
 modal.subscribeState(newState => {
   if (newState.open) {
     disconnect()
-  } else {
-    var account = getAccount()
-    if (account.isConnected) {
-      signMessage({message: 'gm wagmi frens'})
-    }
   }
 })
 
-watchAccount(account => {
-  console.log('watching');
-  document.getElementById('user').innerText = account.address ?? 'Disconnected'
-})
+let links = document.getElementsByClassName('contract-vote')
+for (let i = 0, iLength = links.length; i < iLength; i++) {
+    links[i].addEventListener('click', function(e) {
+      e.preventDefault();
+
+      var _voteType = parseInt(e.target.getAttribute('data-vote'))
+      var account = getAccount()
+      if (account.isConnected) {
+        writeContract({
+          address: contractAddress,
+          account: account,
+          abi: {"name": "vote", "inputs": [{"indexed": true, "internalType": "uint", "name": "_voteType", "type": "uint"}], "outputs": [], "stateMutability": "nonpayable", "type": "function"},
+          functionName: 'vote',
+          args: [_voteType],
+          // abi: [{"inputs":[],"name":"feed","outputs":[],"stateMutability":"nonpayable","type":"function"}],
+          // functionName: 'feed',
+        })
+      }
+    });
+}
