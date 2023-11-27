2c97a022a08c61e74981e890d7f663b944216960
lstephanian <stephanianlm@gmail.com>
2023-11-26 22:55:13 -0600

    updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,24 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+
+// NEVER record important private keys in your code - this is for demo purposes
+const GOERLI_TESTNET_PRIVATE_KEY = process.env.PRIV_KEY;
+const ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY = '';
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.21",
+  solidity: '0.8.20',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    arbitrumGoerli: {
+      url: 'https://goerli-rollup.arbitrum.io/rpc',
+      chainId: 421613,
+      accounts: [GOERLI_TESTNET_PRIVATE_KEY]
+    },
+    arbitrumOne: {
+      url: 'https://arb1.arbitrum.io/rpc',
+      //accounts: [ARBITRUM_MAINNET_TEMPORARY_PRIVATE_KEY]
+    },
+  },
 };
