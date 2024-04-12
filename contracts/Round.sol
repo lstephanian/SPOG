7445fa9db7dea450af0fe5c6d6a9ef4950afa461
lstephanian <stephanianlm@gmail.com>
2024-04-11 21:45:49 -0700

    clean up unused contracts

--- contracts/Round.sol
+++ contracts/Round.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 //author: @lstephanian
 
-pragma solidity ^0.8.9;
+pragma solidity ^ 0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
 contract Round is Ownable(msg.sender) {
-    int votesUninformed = 0;
-    int votesInformed = 0;
-    int votesAbstain = 0;
-    int informedClaimable = 0;
-    int uninformedClaimable = 0;
-    int abstainedClaimable = 0;
+    int256 votesUninformed = 0;
+    int256 votesInformed = 0;
+    int256 votesAbstain = 0;
+    int256 informedClaimable = 0;
+    int256 uninformedClaimable = 0;
+    int256 abstainedClaimable = 0;
     bool public ended = false;
 
     mapping(address => int) private voteMap;
@@ -22,108 +22,114 @@ contract Round is Ownable(msg.sender) {
     event RoundEnded(bool);
     event VoteSubmitted(int, int);
     event VotesTallied(int informedClaimable, int uninformedClaimable, int abstainedClaimable);
-    event BitRefundReceived(address, int);
+    event BitRefundReceived(address, uint);
 
     function closeRound() public onlyOwner {
+        require(ended==false, "round already closed");
         ended = true;
         emit RoundEnded(ended);
     }
 
     function getRoundStatus() public view returns(bool) {
-        return(ended);
+        return (ended);
     }
 
     function vote(int _voteType) public {
-        require(ended==false, "round closed");
+        require(ended == false, "round closed");
         require(_voteType > 0 && _voteType < 4, 'needs to be in range');
         require(voteMap[msg.sender] == 0, 'can only vote once');
 
         voteMap[msg.sender] = _voteType;
 
-        if (_voteType==1){
+        if (_voteType == 1) {
             votesAbstain += 1;
             emit VoteSubmitted(_voteType, votesAbstain);
         }
-        if (_voteType==2){
+        if (_voteType == 2) {
             votesUninformed += 1;
             emit VoteSubmitted(_voteType, votesUninformed);
         }
-        if (_voteType==3) {
+        if (_voteType == 3) {
             votesInformed += 1;
             emit VoteSubmitted(_voteType, votesInformed);
         }
     }
 
     function getVoterStatus(address beneficiary) public view returns(int) {
-        return(voteMap[beneficiary]);
+        return (voteMap[beneficiary]);
     }
 
     function tallyVotesExpensive() public onlyOwner {
+        
+        // here, 1 is same 10 ** 18
         require(ended, 'round is still open');
 
-        int votesTotal = votesInformed + votesUninformed; 
-        require(votesTotal > 0, "no votes submitted");
+        int256 votesTotal = votesInformed + votesUninformed;
+        require(votesTotal > 0, "no non-abstain votes submitted");
 
-        int b = (votesTotal/2 - votesUninformed / votesTotal) * votesInformed / votesTotal; 
-        int v = ((votesTotal - 1) / (votesTotal * votesTotal)) + (1  / votesTotal);
+        int256 b = 10 ** 18 * (votesTotal / 2 - votesUninformed / votesTotal) * votesInformed / votesTotal;
+        int256 v = 10 ** 18 * ((votesTotal - 1) / (votesTotal * votesTotal)) + (1 / votesTotal);
 
         // allows for up to 10**59 votes
-        if (v * 10**18 > 3**18) {
-            v = 3**18;
+        if (v > 3 * 10 ** 18) {
+            v = 3 * 10 ** 18;
         }
 
-        if (b > 1) {
-            informedClaimable = b-1;
+        if (b > 10 ** 18) {
+            informedClaimable = b - 10 ** 18;
         }
         if (b > 0) {
             uninformedClaimable = b;
         }
-        if (b-v > 0) {
-            abstainedClaimable = b * 10**18 > v ? b * 10**18 - v : int(0);
+        if (b - v > 0) {
+            abstainedClaimable = b > v ? b - v : int(0);
         }
         emit VotesTallied(informedClaimable, uninformedClaimable, abstainedClaimable);
     }
-    
+
     function tallyVotesFree() public onlyOwner {
         require(ended, 'round is still open');
 
-        int votesTotal = votesInformed + votesUninformed;
+        int256 votesTotal = votesInformed + votesUninformed;
         require(votesTotal > 0, "no votes submitted");
 
-        int b = (int(votesTotal) / 2 - (int(votesUninformed) / int(votesTotal))) * int(votesInformed) / int(votesTotal);
+        int256 b = 10 ** 18 * (int(votesTotal) / 2 - (int(votesUninformed) / int(votesTotal))) * int(votesInformed) / int(votesTotal);
 
-        if (b > 1) {
-            informedClaimable = b-1;
+        if (b > 10 ** 18) {
+            informedClaimable = b - 10 ** 18;
         }
 
         if (b > 0) {
             uninformedClaimable = b;
             abstainedClaimable = b;
         }
-    
+
         emit VotesTallied(informedClaimable, uninformedClaimable, abstainedClaimable);
     }
 
     function claimFunds() public {
         require(ended, 'round is still open');
         require(withdrawalMap[msg.sender] == false, 'can only withdraw once');
-        int amount;
 
-        if (voteMap[msg.sender] == 1){
-            value = abstainedClaimable;
+        uint claimAmt;
+
+        if (voteMap[msg.sender] == 1) {
+            claimAmt = uint(abstainedClaimable);
         }
-        if (voteMap[msg.sender] == 2){
-            value = votesUninformed;
+        if (voteMap[msg.sender] == 2) {
+            claimAmt = uint(votesUninformed);
         }
-        if (voteMap[msg.sender] == 3){
-            value = votesInformed;
-        }        
-        
+        if (voteMap[msg.sender] == 3) {
+            claimAmt = uint(votesInformed);
+        }
+
+        //indicate msg.sender has withdrawn
         withdrawalMap[msg.sender] = true;
-        
-        (bool sent, bytes memory data) = _to.call{value: msg.value}("");
+
+        //send eth
+        (bool sent, bytes memory data) = msg.sender.call{value: claimAmt}("");
         require(sent, "Failed to send Ether");
-        
-        emit BitRefundReceived(msg.sender, value);
+
+        emit BitRefundReceived(msg.sender, claimAmt);
     }
 }
